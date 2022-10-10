8c785c07edefa563bca14ba115bd1fffe71f130d
Tarik BaBa <comback.tictac@live.fr>
2022-10-10 16:46:13 +0200

    add boostrap & ToDoController ...

---
+++ src/Controller/ToDoControlerController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ToDoControlerController extends AbstractController
+{
+    #[Route('/todo', name: 'todo')]
+    public function index(Request $request): Response
+    {
+        $session = $request->getSession();
+
+        if (!$session->has('todos')) {
+            $todos = [
+                'achat' => 'acheter clé usb',
+                'cours' => 'Finaliser mon cours',
+                'debug' => 'revoir mon code'
+            ];
+
+            $session->set('todos', $todos);
+        }
+
+
+        return $this->render('to_do_controler/index.html.twig');
+    }
+}
