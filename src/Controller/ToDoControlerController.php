d49c9e06256d682788b8c7bf8f980b23d945e3f2
Tarik BaBa <comback.tictac@live.fr>
2022-10-11 11:19:33 +0200

    - Functions flash-message with session - adding new tasks in the array todos

--- src/Controller/ToDoControlerController.php
+++ src/Controller/ToDoControlerController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+// use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,9 +23,32 @@ class ToDoControlerController extends AbstractController
             ];
 
             $session->set('todos', $todos);
+            $this->addFlash('info', "La liste todos viens d'être initialisée");
         }
 
+        return $this->render('to_do_controler/todo.html.twig');
+    }
 
-        return $this->render('to_do_controler/index.html.twig');
+    #[Route('/todo/add/{name}/{content}', name: 'todo_add')]
+    public function addToDo(Request $request, $name, $content)
+
+    {
+        $session = $request->getSession();
+
+        if ($session->has('todos')) {
+            $todos = $session->get('todos');
+
+            if (isset($todos[$name])) {
+                $this->addFlash('error', "Le ToDo d'id $name existe déjà dans la liste");
+            } else {
+                $todos[$name] = $content;
+                $this->addFlash('success', "Le todo d'id $name à été ajouté avec succès");
+                $session->set('todos', $todos);
+            }
+        } else {
+
+            $this->addFlash('error', "La liste n'est pas encore initialisée");
+        }
+        return $this->redirectToRoute('todo');
     }
 }
