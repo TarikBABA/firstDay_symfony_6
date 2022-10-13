5c2a48694e39ff090b86b91c359ff4c836a3c270
Tarik BaBa <comback.tictac@live.fr>
2022-10-13 16:28:49 +0200

    create person in table people

--- src/Controller/PeopleController.php
+++ src/Controller/PeopleController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\People;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PeopleController extends AbstractController
 {
-    #[Route('/people', name: 'app_people')]
-    public function index(): Response
+    #[Route('/people/add', name: 'app_people')]
+    public function addPerson(ManagerRegistry $doctrine): Response
     {
-        return $this->render('people/index.html.twig', [
-            'controller_name' => 'PeopleController',
+        $entityManager = $doctrine->getManager();
+        $person = new People();
+        $person->setFirstname('Anass');
+        $person->setName('BABA');
+        $person->setAge('2');
+
+        //* ajouter l'opération d'insertion de person dans ma transaction
+        $entityManager->persist($person);
+        //* Excuter la transaction #ToDo
+        $entityManager->flush();
+
+        return $this->render('people/detail.html.twig', [
+            'person' => $person,
         ]);
     }
 }
