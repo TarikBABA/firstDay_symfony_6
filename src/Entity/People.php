4eb64c58755fd898e371716cedac41d14b029aa2
Tarik BaBa <comback.tictac@live.fr>
2022-10-17 16:33:30 +0200

    relation entity

--- src/Entity/People.php
+++ src/Entity/People.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\PeopleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -23,8 +25,19 @@ class People
     #[ORM\Column(type: Types::SMALLINT)]
     private ?int $age = null;
 
-    #[ORM\Column(length: 50, nullable: true)]
-    private ?string $job = null;
+    #[ORM\OneToOne(inversedBy: 'people', cascade: ['persist', 'remove'])]
+    private ?profile $profile = null;
+
+    #[ORM\ManyToMany(targetEntity: Hobby::class)]
+    private Collection $Hobbies;
+
+    #[ORM\ManyToOne(inversedBy: 'people')]
+    private ?Job $Job = null;
+
+    public function __construct()
+    {
+        $this->Hobbies = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -67,14 +80,51 @@ class People
         return $this;
     }
 
-    public function getJob(): ?string
+
+    public function getProfile(): ?profile
     {
-        return $this->job;
+        return $this->profile;
     }
 
-    public function setJob(?string $job): self
+    public function setProfile(?profile $profile): self
     {
-        $this->job = $job;
+        $this->profile = $profile;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Hobby>
+     */
+    public function getHobbies(): Collection
+    {
+        return $this->Hobbies;
+    }
+
+    public function addHobby(Hobby $hobby): self
+    {
+        if (!$this->Hobbies->contains($hobby)) {
+            $this->Hobbies->add($hobby);
+        }
+
+        return $this;
+    }
+
+    public function removeHobby(Hobby $hobby): self
+    {
+        $this->Hobbies->removeElement($hobby);
+
+        return $this;
+    }
+
+    public function getJob(): ?Job
+    {
+        return $this->Job;
+    }
+
+    public function setJob(?Job $Job): self
+    {
+        $this->Job = $Job;
 
         return $this;
     }
---
+++ src/Entity/Profile.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProfileRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProfileRepository::class)]
+class Profile
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $url = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $rs = null;
+
+    #[ORM\OneToOne(mappedBy: 'profile', cascade: ['persist', 'remove'])]
+    private ?People $people = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getRs(): ?string
+    {
+        return $this->rs;
+    }
+
+    public function setRs(string $rs): self
+    {
+        $this->rs = $rs;
+
+        return $this;
+    }
+
+    public function getPeople(): ?People
+    {
+        return $this->people;
+    }
+
+    public function setPeople(?People $people): self
+    {
+        // unset the owning side of the relation if necessary
+        if ($people === null && $this->people !== null) {
+            $this->people->setProfile(null);
+        }
+
+        // set the owning side of the relation if necessary
+        if ($people !== null && $people->getProfile() !== $this) {
+            $people->setProfile($this);
+        }
+
+        $this->people = $people;
+
+        return $this;
+    }
+}
