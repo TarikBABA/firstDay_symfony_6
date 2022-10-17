9283e587404047532176b1307225bd338bcaf6fa
Tarik BaBa <comback.tictac@live.fr>
2022-10-17 10:07:44 +0200

    CRUD with (route) :pencil2:

--- src/Controller/PeopleController.php
+++ src/Controller/PeopleController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\People;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,36 +13,36 @@ use Symfony\Component\Routing\Annotation\Route;
 class PeopleController extends AbstractController
 {
     //? Show All people 1 page
-    // #[Route('/', name: 'people')]
-    // public function index(ManagerRegistry $doctrine): Response
-    // {
-    //     $repository = $doctrine->getRepository(People::class);
-    //     $allPeople = $repository->findAll();
-    //     return $this->render('people/index.html.twig', [
-    //         'allPeople' => $allPeople
-    //     ]);
-    // }
+    #[Route('/', name: 'people')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $repository = $doctrine->getRepository(People::class);
+        $people = $repository->findAll();
+        return $this->render('people/index.html.twig', [
+            'isPaginated' => false,
+            'allPeople' => $people
+        ]);
+    }
 
-
-    // ? Show All where condition = true
-    // #[Route('/', name: 'people')]
-    // public function index(ManagerRegistry $doctrine): Response
-    // {
-    //     $repository = $doctrine->getRepository(People::class);
-    //     $allPeople = $repository->findBy(['name' => 'baba'], ['age' => 'DESC']);
-    //     return $this->render('people/index.html.twig', [
-    //         'allPeople' => $allPeople
-    //     ]);
-    // }
+    #[Route('/test', name: 'people.test')]
+    public function indexByName(ManagerRegistry $doctrine): Response
+    {
+        $repository = $doctrine->getRepository(People::class);
+        $people = $repository->findBy(['name' => 'baba'], ['age' => 'DESC']);
+        return $this->render('people/index.html.twig', [
+            'allPeople' => $people,
+            'isPaginated' => false
+        ]);
+    }
 
     // ? Show All with Pagination
-    #[Route('/{page?1}/{nbr?12}', name: 'people')]
-    public function index(ManagerRegistry $doctrine, $nbr, $page): Response
+    #[Route('/pages/{page?1}/{nbr?12}', name: 'people.pagination')]
+    public function indexPagination(ManagerRegistry $doctrine, $nbr, $page): Response
     {
         $repository = $doctrine->getRepository(People::class);
         $nbPeople = $repository->count([]);
         $nbPages = ceil($nbPeople / $nbr);
-        $allPeople = $repository->findBy([], [], $nbr, (($page - 1) * $nbr));
+        $allPeople = $repository->findBy([], [], $nbr, ($page - 1) * $nbr);
         return $this->render('people/index.html.twig', [
             'allPeople' => $allPeople,
             'isPaginated' => true,
@@ -51,26 +52,8 @@ class PeopleController extends AbstractController
         ]);
     }
 
-    // #[Route('/{id<\d+>}', name: 'user')]
-    // public function user(ManagerRegistry $doctrine, $id): Response
-    // {
-    //     $repository = $doctrine->getRepository(People::class);
-    //     $person = $repository->find($id);
-
-    //     if (!$person) {
-    //         $this->addFlash(
-    //             'error',
-    //             "l'id $id n'exsiste pas !"
-    //         );
-    //         return $this->redirectToRoute('people');
-    //     }
-    //     return $this->render('people/user.html.twig', [
-    //         'user' => $person
-    //     ]);
-    // }
-
-    #[Route('/{id<\d+>}', name: 'user')]
-    public function user(People $person): Response
+    #[Route('/{id<\d+>}', name: 'details.user')]
+    public function user(People $person = null): Response
     {
         if (!$person) {
             $this->addFlash(
@@ -107,5 +90,72 @@ class PeopleController extends AbstractController
         return $this->render('people/person.html.twig', [
             'person' => $person,
         ]);
+    }
+
+
+
+    // #[Route('/{id<\d+>}', name: 'user')]
+    // public function user(ManagerRegistry $doctrine, $id): Response
+    // {
+    //     $repository = $doctrine->getRepository(People::class);
+    //     $person = $repository->find($id);
+
+    //     if (!$person) {
+    //         $this->addFlash(
+    //             'error',
+    //             "l'id $id n'exsiste pas !"
+    //         );
+    //         return $this->redirectToRoute('people');
+    //     }
+    //     return $this->render('people/user.html.twig', [
+    //         'user' => $person
+    //     ]);
+    // }
+
+
+
+
+    #[Route('/delete/{id}', name: 'delete.people')]
+    public function deletePerson(People $person = null, ManagerRegistry $doctrine): RedirectResponse
+    {
+
+        if ($person) {
+            $manager = $doctrine->getManager();
+            $manager->remove($person);
+            $manager->flush();
+            $this->addFlash(
+                'success',
+                'la personne a été supprimer avec succès'
+            );
+        } else {
+            $this->addFlash(
+                'error',
+                'person not exist'
+            );
+        }
+        return $this->redirectToRoute('people.pagination');
+    }
+
+    #[Route('/update/{id}/{name}/{firstname}/{age}', name: 'updatePerson')]
+    public function updatePerson(People $person = null, $name, $firstname, $age, ManagerRegistry $doctrine)
+    {
+        if ($person) {
+            $person->setName($name);
+            $person->setFirstname($firstname);
+            $person->setAge($age);
+            $manager = $doctrine->getManager();
+            $manager->persist($person);
+            $manager->flush($person);
+            $this->addFlash(
+                'success',
+                'mise à jour avec succès'
+            );
+        } else {
+            $this->addFlash(
+                'error',
+                'not exist'
+            );
+        }
+        return $this->redirectToRoute('people.pagination');
     }
 }
