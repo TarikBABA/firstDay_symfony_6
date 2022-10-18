1696b3ac33cfd7a1501442d41910dbbbe137056f
Tarik BaBa <comback.tictac@live.fr>
2022-10-18 12:40:11 +0200

    Doctrine lifeCucle call back les evenements de doctrine

--- src/Entity/People.php
+++ src/Entity/People.php
@@ -9,6 +9,8 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PeopleRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+
 class People
 {
     #[ORM\Id]
@@ -26,13 +28,19 @@ class People
     private ?int $age = null;
 
     #[ORM\OneToOne(inversedBy: 'people', cascade: ['persist', 'remove'])]
-    private ?profile $profile = null;
+    private ?Profile $profile = null;
 
     #[ORM\ManyToMany(targetEntity: Hobby::class)]
     private Collection $Hobbies;
 
     #[ORM\ManyToOne(inversedBy: 'people')]
     private ?Job $Job = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTime $createdAt = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $updateAt = null;
 
     public function __construct()
     {
@@ -81,12 +89,12 @@ class People
     }
 
 
-    public function getProfile(): ?profile
+    public function getProfile(): ?Profile
     {
         return $this->profile;
     }
 
-    public function setProfile(?profile $profile): self
+    public function setProfile(?Profile $profile): self
     {
         $this->profile = $profile;
 
@@ -127,5 +135,48 @@ class People
         $this->Job = $Job;
 
         return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdateAt(): ?\DateTimeInterface
+    {
+        return $this->updateAt;
+    }
+
+    public function setUpdateAt(?\DateTimeInterface $updateAt): self
+    {
+        $this->updateAt = $updateAt;
+
+        return $this;
+    }
+
+
+    #[ORM\PrePersist]
+    #[ORM\PreUpdate]
+    public function onPrePersist()
+    {
+        $this->createdAt = new \DateTime();
+        $this->updateAt = new \DateTime();
+    }
+
+
+    #[ORM\PreUpdate]
+    // /**
+    //  * @ORM\PreUpdate
+    //  */
+    public function onPreUpdate()
+    {
+        $this->updateAt = new \DateTime();
     }
 }
